cfd9e45c22a3d4dc5e052581ea8a576911c31bb7
Johan Taborda <johan.david.taborda@gmail.com>
2025-03-29 19:41:33 -0500

    Se agrega mini estructura.

---
+++ src/layouts/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'
+
+const App = () => {
+  
+
+  return (
+    <>
+        Espacio para el to-do list
+    </>
+  )
+}
+
+export default App
---
+++ src/layouts/Main/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Main.css'
+
+const Main = () => {
+  
+
+  return (
+    <>
+        Espacio para el to-do list
+    </>
+  )
+}
+
+export default Main;
